1a80278ce743cdc9c54403e6b53daf5ea77ee36a
Jacob Spade <spade.jacob@Yahoo.com>
2019-03-07 14:22:02 -0800

    fixed mongouri connection

--- server.js
+++ server.js
@@ -2,23 +2,21 @@ const express = require('express');
 const request = require('request');
 const mongoose = require('mongoose');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// api
-const url = 'https://jsonplaceholder.typicode.com/photos?albumId=1'
+// bodyparser middleware
+app.use(bodyParser.json());
+
+// db config
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db)
   .then(() => console.log('mongodb connected'))
   .catch(err => console.log(err));
-
-// set the view engine to ejs
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '../public'));
 
 app.get('/', (req, res) => {
   request(url, (err, reponse, body) => {
